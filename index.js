84b52bd8a245847e55e3dd01f3aa7750d8062aac
minarin0179 <takissyd@gmail.com>
2021-12-30 19:44:59 +0900

    とりあえず起動まで

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-/*
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', () => {
 	console.log('準備完了！');
 });
 
 client.login('OTI2MDUxODkzNzI4NDAzNDg2.Yc2DCA.yy5an1kAKWvMIT0BFjqJPeolakU');
-*/
-console.log('準備完了！');
+
+// console.log('準備完了！');
