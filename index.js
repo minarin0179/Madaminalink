92028a46dc11167660e33126d3505b323377ba01
minarin0179 <takissyd@gmail.com>
2021-12-30 19:07:38 +0900

    bot接続テスト

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('準備完了！');
+});
+
+client.login('OTI2MDUxODkzNzI4NDAzNDg2.Yc2DCA.yy5an1kAKWvMIT0BFjqJPeolakU');
