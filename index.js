583c6919f1bd557511b0df5c1502c7a425c4c345
minarin0179 <takissyd@gmail.com>
2021-12-31 11:02:40 +0900

    categoryコピー対応

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// モジュール読み込み
+// モジュール読み込み\
 const { Client, Intents } = require('discord.js');
 // configを読み込み
 const { prefix, token } = require('./config.json');
@@ -9,6 +9,13 @@ client.once('ready', () => {
     const data = [{
         name: 'copy',
         description: 'チャンネルをメッセージや添付ファイルを含めて複製します',
+        options: [{
+            type: 'CHANNEL',
+            channelTypes: ['GUILD_TEXT', 'GUILD_CATEGORY'],
+            name: 'target',
+            description: 'コピーするチャンネル/カテゴリー',
+            required: true,
+        }],
     }];
     client.application.commands.set(data, '926052259069059102');
     console.log('準備完了！');
@@ -18,29 +25,8 @@ client.on('messageCreate', message => {
     // prefixのないメッセージやbotからのメッセージは無視
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).trim().split(' ');
-    const command = args.shift().toLowerCase();
-
-    if (command === 'ping') {
-        // message.channel.send('pong');
-    }
-    else if (command === 'copy') {
-        message.guild.channels.create('copy ' + message.channel.name, { parent: message.channel.parent }).then((new_channel) => {
-            message.channel.messages.fetch({ before: message.id }).then(messages => {
-                (async () => {
-                    for await (const msg of messages.reverse()) {
-                        await new_channel.send(msg[1].content);
-                        console.log(msg[1].content);
-                        if (msg[1].attachments.size > 0) {
-                            const files = await msg[1].attachments.map(attachment => attachment.url);
-                            await new_channel.send({ files });
-                            console.log(msg[1].attachments);
-                        }
-                    }
-                })();
-            });
-        });
-    }
+    // const args = message.content.slice(prefix.length).trim().split(' ');
+    // const command = args.shift().toLowerCase();
 });
 
 
@@ -49,15 +35,46 @@ client.on('interactionCreate', async (interaction) => {
         return;
     }
     if (interaction.commandName === 'copy') {
+        // 応答時間が長くなる場合
         await interaction.deferReply();
-        interaction.guild.channels.create('copy ' + interaction.channel.name, {
+
+        const original = interaction.options.getChannel('target');
+        if (original.type === 'GUILD_TEXT') {
+            console.log(original);
+            await copyChannel(original, original.parent).then(() => {
+                interaction.followUp({ content: 'コピーは正常に完了しました', ephemeral: true });
+            }).catch(() => {
+                interaction.followUp({ content: 'コピー中にエラーが発生しました\n処理を中断します', ephemeral: true });
+            });
+        }
+        else if (original.type === 'GUILD_CATEGORY') {
+            console.log(original.children);
+            original.guild.channels.create('copy ' + original.name, {
+                type: 'GUILD_CATEGORY',
+                permissionOverwrites: original.permissionOverwrites.cache,
+            }).then(async (new_category) => {
+                for await (const ch of original.children) {
+                    copyChannel(ch[1], new_category);
+                }
+            });
+            interaction.followUp({ content: 'コピーは正常に完了しました', ephemeral: true });
+        }
+    }
+});
+
+// チャンネルを複製して内容をコピー
+const copyChannel = (original, category) => {
+    return new Promise((resolve, reject) => {
+        // チャンネルを作る
+        original.guild.channels.create('copy ' + original.name, {
             // カテゴリー設定
-            parent: interaction.channel.parent,
+            parent: category,
             // 権限をコピー
-            permissionOverwrites: interaction.channel.permissionOverwrites.cache,
+            permissionOverwrites: original.permissionOverwrites.cache,
         }).then((new_channel) => {
-            interaction.channel.messages.fetch({ before: interaction.id }).then(messages => {
-                (async () => {
+            // メッセージをすべて取得
+            original.messages.fetch().then(async (messages) => {
+                try {
                     for await (const msg of messages.reverse()) {
                         if (msg[1].content === '') continue;
                         await new_channel.send(msg[1].content);
@@ -66,13 +83,15 @@ client.on('interactionCreate', async (interaction) => {
                             await new_channel.send({ files });
                         }
                     }
-                    await interaction.followUp({ content: 'コピーは正常に完了しました', ephemeral: true });
-                    //await interaction.deleteReply();
-                })();
+                }
+                catch {
+                    reject();
+                }
+                resolve();
             });
         });
-    }
-});
+    });
+};
 
 
 client.login(token);
